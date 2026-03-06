1ba3b8dcd9f9dffa64dcf78f91dcdfe665527bab
omniofficial <omniofficial04@gmail.com>
2026-03-05 17:36:12 -0800

    Skeleton is done

---
+++ hw2_moviedatabase/script.js
@@ -0,0 +1,23 @@
+function renderMovies() {
+    console.log("renderMovies() ran");
+}
+
+function searchMovies() {
+    console.log("searchMovies() ran");
+}
+
+function sortAscending() {
+    console.log("sortAscending() ran");
+}
+
+function sortDescending() {
+    console.log("sortDescending() ran");
+}
+
+function sortNewest() {
+    console.log("sortNewest() ran");
+}
+
+function sortOldest() {
+    console.log("sortOldest() ran");
+}
